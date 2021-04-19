8ef8bcae7407407789e2cd219808938db324954a
Zane <Zane.Payer@gmail.com>
2021-04-19 19:04:42 +0200

    hello

---
+++ messages.js
@@ -0,0 +1,30 @@
+//return a random message. Fortune teller
+
+
+
+//1. User types sends input
+
+
+
+//2. randomly selects three part string
+    //a. what string parts
+
+    //b. where do strings come from
+    
+    //c. how to showcase JS concepts
+
+
+
+//3. response is returned to console
+
+const tellMeMyFortune = () => {
+    return console.log('outlook hazy try again.')
+
+}
+
+const what = (number) => {
+   return number*number
+}
+
+module.exports.tellMeMyFortune = tellMeMyFortune
+module.exports.what = what;
