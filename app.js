90043f6a6fbc912bd2f4393c4d7a5265c1b7bad1
Zane <Zane.Payer@gmail.com>
2021-04-20 12:30:06 +0200

    restoring big array

--- app.js
+++ app.js
@@ -2,3 +2,6 @@ const messages = require('./messages');
 let result = messages.tellMeMyFortune
 //console.log(messages);
 console.log(messages());
+
+//This is the function to call in terminal. Call by using node app
+//to get a randomized fortune
